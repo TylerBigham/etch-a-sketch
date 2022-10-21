3d5551feaf00d184348c78ea618767ec82f03b69
Tyler Bigham <Tyler.Bigham.Full.Breakfast@gmail.com>
2022-10-21 14:42:37 -0400

    Update createGrid function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 
 const gridContainer = document.querySelector('.gridContainer');
 
+createGrid(16);
 
 function createGrid(num){
     for (let i = 1; i < num; i++){
-
-
+        const grid = document.createElement('div');
+        grid.classList.add('row');
+        grid.textContent = i;
+        gridContainer.appendChild(grid);
+        for (let j = 1; j < num; j++){
+            const grid = document.createElement('div');
+            grid.classList.add('column');
+            grid.textContent = j;
+            gridContainer.appendChild(grid);
+        }
     }
 }

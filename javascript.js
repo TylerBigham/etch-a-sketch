429a108f65798f1de5fde45c3691697674e7d7ec
Tyler Bigham <Tyler.Bigham.Full.Breakfast@gmail.com>
2022-11-04 10:48:19 -0400

    Add button to refresh page, add prompt for grid

--- javascript.js
+++ javascript.js
@@ -1,19 +1,40 @@
 
 const gridContainer = document.querySelector('.gridContainer');
+const resetButton = document.querySelector('.reset');
+//const range = document.getElementById("myRange");
 
-createGrid(16);
+getInput();
+
+/*range.oninput = function() {
+    location.reload();
+    createGrid(range.value);
+}*/
+
+const reloadPage = () => {
+    location.reload();
+}
+
+resetButton.addEventListener('click', reloadPage);
+
+function getInput(){
+    let gridSize = prompt("Enter grid size 10 - 100");
+    if (gridSize >= 10 && gridSize <= 100){
+        createGrid(gridSize);
+    }else{
+        getInput();
+    }
+}
 
 function createGrid(num){
-    for (let i = 1; i < num; i++){
+    for (let i = 0; i < num; i++){
         const grid = document.createElement('div');
         grid.classList.add('row');
-        grid.textContent = i;
         gridContainer.appendChild(grid);
-        for (let j = 1; j < num; j++){
-            const grid = document.createElement('div');
-            grid.classList.add('column');
-            grid.textContent = j;
-            gridContainer.appendChild(grid);
+        for (let j = 0; j < num; j++){
+            const column = document.createElement('div');
+            column.classList.add('column');
+            column.textContent = '';
+            grid.appendChild(column);
         }
     }
 }
